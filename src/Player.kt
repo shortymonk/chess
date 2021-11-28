8df90e7363b09d08e70c789778f91200dae8cf51
Bish <shortymonk@gmail.com>
2021-11-28 17:14:59 +0400

    Pawns-only_chess_stage_4 Architecture is changed to singleton

--- src/Player.kt
+++ src/Player.kt
@@ -1,11 +1,71 @@
-open class Player(val color: String = "white", val name: String,val chess: Board) {
-    val pawnSet = if (color == "white") chess.whitePawns else chess.blackPawns
-    val opponentSet = if (color == "white") chess.blackPawns else chess.whitePawns
-    val initPosition = if (color == "white") 2 else 7
-    fun convert(x: Char): Int {
-        return when (x) {
-            'a' -> 1; 'b' -> 2; 'c' -> 3; 'd' -> 4
-            'e' -> 5; 'f' -> 6; 'g' -> 7; else -> 8
+class Player(val color: String = "white", val name: String) {
+
+    companion object {
+        val whitePawns = mutableSetOf<Pair<Int, Int>>()
+        val blackPawns = mutableSetOf<Pair<Int, Int>>()
+        init { for (i in 1..8) { whitePawns += Pair(i, 2); blackPawns += Pair(i, 7) } }
+        var isWhite: Boolean = true
+        var chance: Pair<Int, Int> = Pair(0, 0)
+
+        fun newPlayer(): Player {
+            val color = if (isWhite) "white" else "black"
+            println("${if (isWhite) "First" else "Second"} Player's name:")
+            val name = readLine()!!
+            isWhite = !isWhite
+            return Player(color, name)
+        }
+
+        fun convert(x: Char): Int {
+            return when (x) {
+                'a' -> 1; 'b' -> 2; 'c' -> 3; 'd' -> 4
+                'e' -> 5; 'f' -> 6; 'g' -> 7; else -> 8
+            }
+        }
+    }
+
+    object Pawns {
+
+        fun move(player: Player,input: String, chess: Board) {
+            val pawnSet = if (isWhite) whitePawns else blackPawns
+            val opponentSet = if (isWhite) blackPawns else whitePawns
+            val take = Pair(convert(input[0]), input[1].toString().toInt())
+            val put = Pair(convert(input[2]), input[3].toString().toInt())
+            val makeStep = {
+                pawnSet.remove(take); pawnSet += put
+                chess.refreshBoard(); isWhite = !isWhite
+            }
+            val pawnIsExist = pawnSet.contains(take)
+            val direction = if (isWhite) 1 else -1
+            val firstStep = if (take.second == if (isWhite) 2 else 7) 2 else 1
+            val checkStep = (put.second - take.second) * direction in 1..firstStep && put != take &&
+                    put.first in (take.first - 1)..(take.first + 1)
+            val isFreeAhead = !(pawnSet + opponentSet).contains(put)
+            val isCapture = opponentSet.contains(put) && (put.second - take.second) * direction == 1 && checkStep
+            val enPassant = chance == Pair(put.first, put.second - 1 * direction) && opponentSet.contains(chance)
+
+            when {
+                !pawnIsExist -> { println("No ${player.color} pawn at ${input.substring(0, 2)}") }
+
+                checkStep && isFreeAhead && take.first == put.first -> {
+                    if ((put.second - take.second) * direction == 2) chance = put else {
+                        chance = Pair(0, 0)
+                    }
+                    makeStep.invoke()
+                }
+
+                isCapture && checkStep && opponentSet.contains(put) && take.first != put.first ->  {
+                    opponentSet.remove(put); makeStep.invoke(); chance = Pair(0, 0)
+                }
+
+                enPassant && checkStep -> {
+                    opponentSet.remove(chance); makeStep.invoke(); chance = Pair(0, 0)
+                }
+
+                else -> println("Invalid Input")
+            }
+//            println("direction: $direction; first step: $firstStep; checkStep: $checkStep; free ahead: $isFreeAhead; capture: $isCapture \n" +
+//                    "Player ${player.color}: $whitePawns \n" +
+//                    "Player ${player.color}: $blackPawns")
         }
     }
 }
--- src/main.kt
+++ src/main.kt
@@ -1,22 +1,19 @@
 fun main() {
     println("Pawns-only chess")
     val chess = Board()
-    println("First Player's name:")
-    val white = Pawns("white", readLine()!!, chess)
-    println("Second Player's name:")
-    val black = Pawns("black", readLine()!!, chess)
+    val white = Player.newPlayer()
+    val black = Player.newPlayer()
     var input: String
     val regex = Regex("[a-h][1-8][a-h][1-8]")
-    println(chess)
+    chess.refreshBoard()
     while (true) {
-        println("${if (chess.turn) white.name else black.name}'s turn:")
+        println("${if (Player.isWhite) white.name else black.name}'s turn:")
         input = readLine()!!
         when {
-            input.matches(regex) && chess.turn -> white.move(input)
-            input.matches(regex) && !chess.turn -> black.move(input)
+            input.matches(regex) && Player.isWhite -> Player.Pawns.move(white, input, chess)
+            input.matches(regex) && !Player.isWhite -> Player.Pawns.move(black, input, chess)
             input == "exit" -> {println("Bye!"); break}
             else -> println("Invalid Input")
         }
-
     }
 }
